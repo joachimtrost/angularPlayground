8c2984181a33df1d5bfacad22bee084c972d9f1b
Joachim Trost <joachim.trost@bluewin.ch>
2018-05-16 13:48:13 +0200

    after chap 4

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,14 +8,6 @@ import { HEROES } from '../mock-heroes';
     styleUrls: ['./heroes.component.css']
 } )
 export class HeroesComponent implements OnInit {
-    /*
-    hero = 'Windstorm';
-    */
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-
     heroes = HEROES;
     selectedHero: Hero;
 
